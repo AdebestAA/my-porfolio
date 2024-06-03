ee2a754611d7b1a483ef8f8c9d6ef31678ad8da9
Adebest <aadebesta@gmail.com>
2024-06-03 10:16:07 +0100

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import Navbar from "./components/Navbar"
+
+
+function App() {
+
+  return (
+    <>
+    <Navbar/>
+    <h1>lmao</h1>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,13 @@
+
+
+const Navbar = () => {
+  return (
+  <nav className="navbar">
+    <button className="nav-btn">skills</button>
+    <button className="nav-btn">⚫</button>
+    <button className="nav-btn">projects</button>
+  </nav>
+  )
+}
+
+export default Navbar
