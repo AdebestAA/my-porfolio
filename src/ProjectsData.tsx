96a266a2fe44dd15b22189d812a0dbd8825e100d
Adebest <aadebesta@gmail.com>
2024-06-24 19:01:02 +0100

    something new

--- src/ProjectsData.tsx
+++ src/ProjectsData.tsx
@@ -6,7 +6,7 @@ export const ProjectsData:projectsDataType[] = [
         id:1,
         name:"Social Lite",
         madeWith:"TypeScript",
-        about:"Social lite is a full stack social media platform that allows user to do what they can do on other social media platforms like facebook,twitter,etc. Users can create accounts using their email, post there pictures with captions,comment on posts and also like posts.Users can also chat with other active users on the platform and social lite also has various groupchat rooms where users can meet and discuss different topics form tech,sport,music etc...",
+        about:"Social lite is a full stack social media platform that allows user to do what they can do on other social media platforms like facebook,twitter,etc. Users can create accounts using their email, post their pictures with captions,comment on posts and also like posts.Users can also chat with other active users on the platform and Social-lite also has various groupchat rooms where users can meet and discuss about different topics related to tech,sport,music etc...",
         tools:["Next js","Typescript","Tailwind CSS","Firebase"],
         link:"https://social-lite-demo.vercel.app/",
         github:"https://github.com/AdebestAA/social-lite"
@@ -15,7 +15,7 @@ export const ProjectsData:projectsDataType[] = [
          id:2,
         name:"food recipe ",
         madeWith:"JavaScript",
-        about:"This is a project built with javascript.in this website you can search for your favorite recipe click on the datail button below each items to see more about the recipe,you can also add any recipe you like to your favourite and also remove them when you like",
+        about:"This is a project built with javascript.in this website you can search for your favorite recipe ,click on the detail button below each items to see more about the recipe, you can also add any recipe you like to your favorite and also remove them whenever you like",
         tools:["React js","JavaScript","Tailwind CSS"],
         link:"https://food-recipe-fit.vercel.app/",
         github:"https://github.com/AdebestAA/food-recipe-REACT"
@@ -33,7 +33,7 @@ export const ProjectsData:projectsDataType[] = [
          id:4,
         name:"Quiz App",
         madeWith:"JavaScript",
-        about:"A fun quiz app where players have 15 seconds to answer each given question.players get a point for every correctly answered question and get no point for every wrongly answered question,players also get no point if they fail to choose an answer before the 15secs countdown timer gets to zero.players will see their scores when they are done with all questions",
+        about:"A fun quiz app where players have 15 seconds to answer each given question. Players get a point for any correctly answered question and get no point for any wrongly answered question,players also get no point if they fail to choose an answer before the 15 seconds countdown timer gets to zero.Players will see their scores when they are done with all questions",
         tools:["HTML","CSS","JavaScript"],
         link:"https://this-quiz-project.vercel.app/",
         github:"https://github.com/AdebestAA/QuizProject"
