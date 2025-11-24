a81302f9908e3fea7913e3b7c208bb4b4206c967
AdebestAA <aadebesta@gmail.com>
2025-11-24 18:35:01 +0100

    okay

--- src/App.tsx
+++ src/App.tsx
@@ -49,7 +49,7 @@ function App() {
       <a href="https://playground.adebest.com/" target="_blank"  className="text-red-500 font-semibold">playground</a>
 <button onClick={toggleTheme} className="text-textOne transition-all ease-in-out duration-500">{theme == "light" ? <Moon /> : <Sun />  }</button>
     </header>
-    <article className="my-4">
+    <article className="mb-4 mt-10">
     <h1 className=" font-semibold">Hi there 👋, I'm Adebest and I enjoy building stuff😉</h1>
 
 <div className="my-6">
@@ -57,9 +57,7 @@ function App() {
  <p> Nice to have you in my corner of the Internet where I showcase what I have built, what I'm currently building and also my skills.</p>
  <p>I am <span className="font-semibold">Adeniran Adeseun </span> FrontEnd Web developer that builds visually appealing, high-performance, highly optimized and user-friendly websites</p>
 <br />
-<br />
  <p> Quick one, years ago when I was just starting out in web development, I built this quiz project and I would like you to check it out, trust me it's wouldn't take much of your time😁😁..it's a country-related/geography-based quiz that you would really enjoy. Cick <a  target='_blank' href="https://this-quiz-project.vercel.app/" className="text-red-500">here</a> to check it out.</p>
-<br />
 <br />
 <p>  Growing up as a kid, I really loved animations and visual effects and as you can guess the love hasn't faded away and that's the reason I created a dedicated website to be my digital playground.The website will be  showcasing the smooth, fun, clean,and web-based animations and visual effects I have worked on and would be working on. Cick <a  target='_blank' href="https://playground.adebest.com" className="text-red-500">here</a> to check out my  playground😜</p>
 
--- src/components/tech-and-skils.tsx
+++ src/components/tech-and-skils.tsx
@@ -3,12 +3,12 @@
 const skills = ["HTML/CSS","Javascript","Typescript","ReactJs","NextJs","Tailwind CSS","GSAP","MongoDB","Hygraph","Firebase","NodeJs","ExpressJs"]
 const TechAndSkills = () => {
   return (
-    <div>
+    <div className="mt-6" >
  <h1 className="text-center font-bold">TECHNOLOGIES / SKILLS</h1>
         <div className='text-center'>
 
         {skills.map((skill,index)=>{
-            return <span key={skill} className='font-semibold text-lg'> {skill} {index +1 == skills.length ? "" : "|"} </span>
+            return <span key={skill} className=' text-lg'> {skill} {index +1 == skills.length ? "" : "|"} </span>
             
         })}
         </div>
