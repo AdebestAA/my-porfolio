463ae56106c9bb079f7aecf6274ac430c3f2d3f9
AdebestAA <aadebesta@gmail.com>
2025-11-27 12:21:01 +0100

    fixed

--- src/App.tsx
+++ src/App.tsx
@@ -57,7 +57,7 @@ function App() {
  <p> Nice to have you in my corner of the Internet where I showcase what I have built, what I'm currently building and also my skills.</p>
  <p>I am <span className="font-semibold">Adeniran Adeseun </span>, a FrontEnd Web developer that builds visually appealing, high-performance, highly optimized and user-friendly websites</p>
 <br />
- <p> Quick one, years ago when I was just starting out in web development, I built this quiz project and I would like you to check it out, trust me it wouldn't take much of your time😁😁..it's a country-related/geography-based quiz that you would really enjoy. Cick <a  target='_blank' href="https://this-quiz-project.vercel.app/" className="text-red-500">here</a> to check it out.</p>
+ <p> Quick one, years ago when I was just starting out in web development, I built this quiz project and I would like you to check it out, trust me, it wouldn't take much of your time😁😁..it's a country-related/geography-based quiz that you would really enjoy. Cick <a  target='_blank' href="https://this-quiz-project.vercel.app/" className="text-red-500">here</a> to check it out.</p>
 <br />
 <p>  Growing up as a kid, I really loved animations and visual effects and as you can guess the love hasn't faded away and that's the reason I created a dedicated website to be my digital playground.The website will be  showcasing the smooth, fun, clean,and web-based animations and visual effects I have worked on and would be working on. Cick <a  target='_blank' href="https://playground.adebest.com" className="text-red-500">here</a> to check out my  playground😜</p>
 
