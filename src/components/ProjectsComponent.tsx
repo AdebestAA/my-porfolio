dac4d105f7ee098cc11d423442f60d6e24fbe758
AdebestAA <aadebesta@gmail.com>
2026-02-05 23:26:11 +0100

    some changes

--- src/components/ProjectsComponent.tsx
+++ src/components/ProjectsComponent.tsx
@@ -1,93 +1,93 @@
-
-
 const projectLists = [
-    {
-        id:8,
-        logo:"streetops.png",
-        title:"StreetOps",
-        about:"The backbone for your grind",
-        link:"https://blogs.streetops.ng/"
-    },
-    {
-        id:1,
-        logo:"/experts-corner.png",
-        title:"Expert's Corner",
-        about:"An encyclopedia for medical professionals",
-        link:"https://ec.orakle.digital/"
-    },
-    {
-        id:2,
-        logo:"/vest-logo.png",
-        title:"VestVale Estate",
-        about:"A visionary real estate development company",
-        link:"https://vestvaleestate.com/"
-    },
-    {
-        id:3,
-        logo:"",
-        title:"Admin Dashboard",
-        about:"A dashboard with different charts",
-        link:"https://admin-dashboard-xyz.vercel.app/"
-    },
-    {
-        id:4,
-        logo:"/vechtron-logo-two.svg",
-        title:"Vechtron",
-        about:"Ai-powered Assistant for cars",
-        link:"https://vechtron-build.vercel.app/"
-    },
-    {
-        id:5,
-        logo:"",
-        title:"Paige",
-        about:"********",
-        link:"https://paige-demo-seven.vercel.app/"
-    },
-    {
-        id:6,
-        logo:"",
-        title:"Medscape",
-        about:"Patient Case Simulations",
-        link:"https://medscape.vercel.app/"
-    },
-    {
-        id:7,
-        logo:"",
-        title:"Food Reecipe",
-        about:"find your favourite recipe",
-        link:"https://food-recipe-fit.vercel.app/"
-    },
-]
+  {
+    id: 8,
+    logo: "streetops.png",
+    title: "StreetOps",
+    about: "The backbone for your grind",
+    link: "https://streetops.ng/",
+  },
+  {
+    id: 1,
+    logo: "/experts-corner.png",
+    title: "Expert's Corner",
+    about: "An encyclopedia for medical professionals",
+    link: "https://ec.orakle.digital/",
+  },
+  {
+    id: 2,
+    logo: "/vest-logo.png",
+    title: "VestVale Estate",
+    about: "A visionary real estate development company",
+    link: "https://vestvaleestate.com/",
+  },
+  {
+    id: 3,
+    logo: "",
+    title: "Admin Dashboard",
+    about: "A dashboard with different charts",
+    link: "https://admin-dashboard-xyz.vercel.app/",
+  },
+  {
+    id: 4,
+    logo: "/vechtron-logo-two.svg",
+    title: "Vechtron",
+    about: "Ai-powered Assistant for cars",
+    link: "https://vechtron-build.vercel.app/",
+  },
+  {
+    id: 5,
+    logo: "",
+    title: "Paige",
+    about: "********",
+    link: "https://paige-demo-seven.vercel.app/",
+  },
+  {
+    id: 6,
+    logo: "",
+    title: "Medscape",
+    about: "Patient Case Simulations",
+    link: "https://medscape.vercel.app/",
+  },
+  {
+    id: 7,
+    logo: "",
+    title: "Food Reecipe",
+    about: "find your favourite recipe",
+    link: "https://food-recipe-fit.vercel.app/",
+  },
+];
 const ProjectsComponent = () => {
   return (
-    <article className='mt-8'>
-<h1 className='text-center font-bold pb-4'>PROJECTS</h1>
+    <article className="mt-8">
+      <h1 className="text-center font-bold pb-4">PROJECTS</h1>
 
-
-<section className='flex flex-col gap-y-3'>
-    {
-        projectLists.map((item,index)=>{
-
-            return <a href={item.link} className='inline-block'  target='_blank' key={index + 1}>
-<div className='flex gap-x-4 border-borderOne border-1 rounded-md w-full p-2 '>
-    <figure className='border-borderOne border-1  rounded-md w-[40px] h-[40px] flex items-center justify-center'>
-<img src={item.logo ? item.logo : "/not-available.png"} alt={item.title} />
-
-    </figure>
-<div>
-    <h1 className='font-semibold text-sm'>{item.title}</h1>
-    <p className='text-sm'>{item.about}</p>
-</div>
-
-</div>
+      <section className="flex flex-col gap-y-3">
+        {projectLists.map((item, index) => {
+          return (
+            <a
+              href={item.link}
+              className="inline-block"
+              target="_blank"
+              key={index + 1}
+            >
+              <div className="flex gap-x-4 border-borderOne border-1 rounded-md w-full p-2 ">
+                <figure className="border-borderOne border-1  rounded-md w-[40px] h-[40px] flex items-center justify-center">
+                  <img
+                    src={item.logo ? item.logo : "/not-available.png"}
+                    alt={item.title}
+                  />
+                </figure>
+                <div>
+                  <h1 className="font-semibold text-sm">{item.title}</h1>
+                  <p className="text-sm">{item.about}</p>
+                </div>
+              </div>
             </a>
-        })
-    }
-</section>
-
-
+          );
+        })}
+      </section>
     </article>
-  )
-}
+  );
+};
 
-export default ProjectsComponent
+export default ProjectsComponent;
--- src/components/tech-and-skils.tsx
+++ src/components/tech-and-skils.tsx
@@ -1,19 +1,37 @@
-
-
-const skills = ["HTML/CSS","Javascript","Typescript","ReactJs","NextJs","Tailwind CSS","GSAP","MongoDB","Hygraph","Firebase","NodeJs","ExpressJs"]
+const skills = [
+  "HTML/CSS",
+  "Javascript",
+  "Typescript",
+  "ReactJs",
+  "NextJs",
+  "Tailwind CSS",
+  "GSAP",
+  "MongoDB",
+  "Hygraph",
+  "Firebase",
+  "NodeJs",
+  "ExpressJs",
+  "Supabase",
+];
 const TechAndSkills = () => {
   return (
-    <div className="mt-6 " >
- <h1 className="text-center font-bold pb-8">TECHNOLOGIES / SKILLS</h1>
-        <section className='text-center flex flex-wrap gap-y-3 justify-between items-center '>
-
-        {skills.map((skill,index)=>{
-            return <div key={index}  className={`md:w-[40%] text-[0.9rem] w-[45%]  text-start border-1 border-borderOne px-2 rounded-sm font-semibold`}> {skill} </div>
-            
+    <div className="mt-6 ">
+      <h1 className="text-center font-bold pb-8">TECHNOLOGIES / SKILLS</h1>
+      <section className="text-center flex flex-wrap gap-y-3 justify-between items-center ">
+        {skills.map((skill, index) => {
+          return (
+            <div
+              key={index}
+              className={`md:w-[40%] text-[0.9rem] w-[45%]  text-start border-1 border-borderOne px-2 rounded-sm font-semibold`}
+            >
+              {" "}
+              {skill}{" "}
+            </div>
+          );
         })}
-        </section>
+      </section>
     </div>
-  )
-}
+  );
+};
 
-export default TechAndSkills
+export default TechAndSkills;
